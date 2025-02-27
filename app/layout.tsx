b964af7849b975e3bae2e377b8cbedd99a82f73e
677000 <1592713356@qq.com>
2025-02-27 22:37:21 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import { ThemeProvider } from "@/components/theme-provider"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Modern Website",
+  title: "Cyan3dScanningSolutions",
   description: "A modern website built with Next.js",
     generator: 'v0.dev'
 }
